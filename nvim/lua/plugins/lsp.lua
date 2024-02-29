d71aa9ff5af0b556ec3b672fd308a23485d35baf
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-29 04:56:02 +0200

    remove redundant stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,60 +1,7 @@
---
--- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
--- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
--- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
--- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
--- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
--- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
---
--- File: plugins/lsp.lua
--- Description: LSP setup and config
--- Author: Kien Nguyen-Tuan <kiennt2609@gmail.com>
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 return {{
-    -- Mason
-    "williamboman/mason.nvim",
-    cmd = {"Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog"},
-    opts = {
-        PATH = "prepend",
-        ui = {
-            icons = {
-                package_pending = " ",
-                package_installed = "󰄳 ",
-                package_uninstalled = " 󰚌"
-            },
-
-            keymaps = {
-                toggle_server_expand = "<CR>",
-                install_server = "i",
-                update_server = "u",
-                check_server_version = "c",
-                update_all_servers = "U",
-                check_outdated_servers = "C",
-                uninstall_server = "X",
-                cancel_installation = "<C-c>"
-            }
-        },
-
-        max_concurrent_installers = 10
-    },
-    config = function(_, opts)
-        require("mason").setup(opts)
-    end
-}, {
-    -- LSP - Quickstart configs for Nvim LSP
     "neovim/nvim-lspconfig",
     event = {"BufReadPre", "BufNewFile"},
     lazy = true,
-    dependencies = { -- Mason
-    -- Portable package manager for Neovim that runs everywhere Neovim runs.
-    -- Easily install and manage LSP servers, DAP servers, linters, and formatters.
-    {"williamboman/mason.nvim"}, {"williamboman/mason-lspconfig.nvim"}, -- Autocomplete
-    -- A completion plugin for neovim coded in Lua.
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {"L3MON4D3/LuaSnip", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-path", "hrsh7th/cmp-buffer",
-                        "saadparwaiz1/cmp_luasnip"}
-    }},
     opts = {
         -- Automatically format on save
         autoformat = true,
@@ -108,36 +55,6 @@ return {{
             require("lspconfig")[server].setup(server_opts)
         end
 
-        -- temp fix for lspconfig rename
-        -- https://github.com/neovim/nvim-lspconfig/pull/2439
-        local mappings = require("mason-lspconfig.mappings.server")
-        if not mappings.lspconfig_to_package.lua_ls then
-            mappings.lspconfig_to_package.lua_ls = "lua-language-server"
-            mappings.package_to_lspconfig["lua-language-server"] = "lua_ls"
-        end
-
-        local mlsp = require("mason-lspconfig")
-        local available = mlsp.get_available_servers()
-
-        local ensure_installed = {} ---@type string[]
-        for server, server_opts in pairs(servers) do
-            if server_opts then
-                server_opts = server_opts == true and {} or server_opts
-                -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-                if server_opts.mason == false or not vim.tbl_contains(available, server) then
-                    setup(server)
-                else
-                    ensure_installed[#ensure_installed + 1] = server
-                end
-            end
-        end
-
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = ensure_installed,
-            automatic_installation = true
-        })
-        require("mason-lspconfig").setup_handlers({setup})
     end
 }, {
     -- load luasnips + cmp related in insert mode only
